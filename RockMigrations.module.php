8e93f63f2f5f0386bbea0521d32ef091c15e14a3
BernhardBaumrock <office@baumrock.com>
2022-01-19 10:31:31 +0100

    add support for auto-run migrations

--- RockMigrations.module.php
+++ RockMigrations.module.php
@@ -6,10 +6,21 @@
  */
 class RockMigrations extends WireData implements Module {
 
+  const cachename = 'rockmigrations-last-run';
+
+  /**
+   * Timestamp of last run migration
+   * @var int
+   **/
+  private $lastrun;
+
+  /** @var WireData */
+  private $watchlist;
+
   public static function getModuleInfo() {
     return [
       'title' => 'RockMigrations',
-      'version' => '0.0.1',
+      'version' => '0.0.2',
       'summary' => 'Brings easy Migrations/GIT support to ProcessWire',
       'autoload' => 2,
       'singular' => true,
@@ -19,9 +30,20 @@ class RockMigrations extends WireData implements Module {
     ];
   }
 
+  public function __construct() {
+    parent::__construct();
+    $this->watchlist = $this->wire(new WireData());
+    $this->watch($this->wire->config->paths->site."migrate.yaml");
+    $this->watch($this->wire->config->paths->site."migrate.php");
+  }
+
   public function init() {
     $this->wire('rockmigrations', $this);
     $this->rm1(); // load RM1 (install it)
+  }
+
+  public function ready() {
+    $this->migrateWatchfiles();
   }
 
   /**
@@ -37,10 +59,92 @@ class RockMigrations extends WireData implements Module {
   }
 
   /**
+   * Get lastmodified timestamp of watchlist
+   * @return int
+   */
+  public function lastmodified() {
+    $last = 0;
+    foreach($this->watchlist as $path=>$options) {
+      if(!is_file($path)) continue;
+      $m = filemtime($path);
+      if($m>$last) $last=$m;
+    }
+    return $last;
+  }
+
+  /**
+   * Run migrations of all watchfiles
+   * @return void
+   */
+  public function migrateWatchfiles() {
+    $lastmodified = $this->lastmodified();
+    $lastrun = $this->wire->cache->get(self::cachename);
+    if($lastrun < $lastmodified) {
+      $this->log('Running migrations...');
+      $this->wire->cache->save(self::cachename, time(), WireCache::expireNever);
+      // bd($this->watchlist);
+      foreach($this->watchlist as $path=>$options) {
+        if(!$options['migrate']) continue;
+        $this->log("Migrating $path");
+        $migrate = $this->wire->files->render($path, $options['vars']);
+        $this->migrate($migrate);
+      }
+    }
+  }
+
+  /**
+   * Return old version of RockMigrations
+   * Will be dropped with RM v1.0.0!
    * @return RockMigrations1
    */
   public function rm1() {
     return $this->wire->modules->get("RockMigrations1");
+  }
+
+  /**
+   * Add file to watchlist
+   *
+   * Usage:
+   * $rm->watch(__FILE__);
+   *
+   * Only watch the file but don't migrate it. This is useful if a migration
+   * file depends on something else (like constants of a module). To make the
+   * migrations run when the module changes you can add the module file to the
+   * watchlist:
+   * $rm->watch('/site/modules/MyModule.module.php', false);
+   *
+   * @param string $file Path to file to be watched for changes
+   * @param bool $migrate Does the file return an array for $rm->migrate() ?
+   * @param array $vars Array of variables to pass to the migrations file
+   * @return void
+   */
+  public function watch($file, $migrate = true, $vars = []) {
+    if(!is_file($file)) return;
+
+    // default variables
+    $defaults = [
+      'rm' => $this,
+    ];
+
+    $path = Paths::normalizeSeparators(realpath($file));
+    $options = [
+      'migrate' => $migrate,
+      'vars' => array_merge($defaults, $vars),
+    ];
+    $this->watchlist->set($path, $options);
+  }
+
+  public function __debugInfo() {
+    $lastrun = "never";
+    if($this->lastrun) {
+      $lastrun = date("Y-m-d H:i:s", $this->lastrun);
+      $lastrun = $this->lastrun." ($lastrun)";
+    }
+    return [
+      'Version' => $this->getModuleInfo()['version'],
+      'lastrun' => $lastrun,
+      'watch' => $this->watchlist,
+    ];
   }
 
 }
