2ee17d91ea9e784d5b5c731bfe43c8925f7991fc
BernhardBaumrock <office@baumrock.com>
2022-10-19 15:45:27 +0200

    update stub

--- Api/stubs/Ping.php
+++ Api/stubs/Ping.php
@@ -17,9 +17,15 @@ class RockMigrations
 
   /**
    * Returns the "foo" property of the rockmigrations instance
+   *
+   * Usage: Add this to /site/ready.php
+   * $rm = $rockmigrations;
+   * $rm->foo = 'I am foo!';
+   * bd($rm->foo());
    */
   public function foo()
   {
-    return $this->foo;
+    // return $this->foo; // this would return null!
+    return $this->rm()->foo;
   }
 }
