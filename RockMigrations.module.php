29fdc33976cf511ff6bd03dc30f58df15d344d43
BernhardBaumrock <office@baumrock.com>
2022-10-19 15:32:59 +0200

    bump version

--- RockMigrations.module.php
+++ RockMigrations.module.php
@@ -74,7 +74,7 @@ class RockMigrations extends WireData implements Module, ConfigurableModule
   {
     return [
       'title' => 'RockMigrations',
-      'version' => '2.1.0',
+      'version' => '2.1.1',
       'summary' => 'The Ultimate Automation and Deployment-Tool for ProcessWire',
       'autoload' => 2,
       'singular' => true,
