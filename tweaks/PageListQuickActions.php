fa8f857cdf091bc571a9aa24cc3430cf0f82c673
BernhardBaumrock <office@baumrock.com>
2023-03-23 13:12:59 +0100

    feat: add PageListQuickActions tweak

---
+++ tweaks/PageListQuickActions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace RockMigrations\Tweaks;
+
+use ProcessWire\HookEvent;
+
+class PageListQuickActions extends Tweak
+{
+  public $description = "Shows action buttons instantly on row hover for superusers";
+
+  public function ready()
+  {
+    if (!$this->wire->user->isSuperuser()) return;
+    if ($this->wire->page->template != 'admin') return;
+    $this->wire->addHookAfter("Page(template=admin)::render", $this, "addStyle");
+  }
+
+  public function addStyle(HookEvent $event)
+  {
+    $event->return = str_replace(
+      "</head>",
+      "<style>
+      /* PageListQuickActions */
+      .PageListActions {
+        transition: opacity 0.3s ease;
+      }
+      .PageListItem:hover > .PageListActions {
+        display:inline !important;
+        opacity:1 !important;
+      }
+      </style></head>",
+      $event->return
+    );
+  }
+}
